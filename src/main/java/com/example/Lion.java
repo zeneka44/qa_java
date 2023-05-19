91bbc838527a62b96e4ab7aa6dcdaeabc97f15a5
zeneka44 <zeneka44@gmail.com>
2023-05-19 13:15:42 +0200

    Commit_1

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,18 +5,18 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
+    Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
+        this.feline = feline;
     }
-
-    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
